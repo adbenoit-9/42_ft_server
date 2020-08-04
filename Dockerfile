6bf8443ea2f83b76ed4a1c0059292aef2cfaafa9
Adeline Benoit <Adeline@MacBook-Air-de-Adeline.local>
2020-08-04 17:03:43 +0200

    not working

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,35 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Dockerfile                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/08/04 00:40:04 by adbenoit          #+#    #+#              #
+#    Updated: 2020/08/04 01:11:21 by adbenoit         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
 
+FROM debian:buster
+
+COPY ./srcs/sh_unzip.sh srcs/
+COPY ./srcs/sh_run.sh srcs/
+COPY ./srcs/sh_ssl.sh srcs/
+COPY ./srcs/sh_default.sh srcs/
+COPY ./srcs/sh_onoff.sh srcs/
+COPY ./srcs/init_mysql.sql srcs/
+COPY ./srcs/index.htm srcs/
+COPY ./srcs/latest.tar.gz var/www/html/
+COPY ./srcs/phpmyadmin.tar var/www/html/
+
+COPY ./srcs/nginx.conf etc/nginx/nginx.conf
+COPY ./srcs/defaulton /etc/nginx/sites-available/default
+COPY ./srcs/defaulton srcs/
+COPY ./srcs/defaultoff srcs/
+
+RUN apt-get update && apt-get upgrade -y
+RUN apt-get install nginx default-mysql-server php php-mbstring php-fpm php-mysql openssl nano -y
+
+EXPOSE 80
+EXPOSE 443
+ENTRYPOINT bash srcs/sh_run.sh
--- install_docker.sh
+++ install_docker.sh
@@ -1,21 +1,23 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    config.sh                                          :+:      :+:    :+:    #
+#    install_docker.sh                                  :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/03 17:59:58 by adbenoit          #+#    #+#              #
-#    Updated: 2020/08/03 19:32:54 by adbenoit         ###   ########.fr        #
+#    Updated: 2020/08/04 01:05:56 by adbenoit         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-
-[ -z "${USER}" ] && export USER='adbenoit'
-docker_path="/goinfre/$USER/docker"
-
-################################## Docker ######################################
 
 # 1. Uninstall is it is intalled with brew
 brew uninstall -f docker
 
 # 2. Install
+# check if Docker is already installed
+while [ ! -d "/Application/Docker.app/" ] && [ ! -d "~/Applications/Docker.app/" ]
+    do
+    echo "\n\033[1mPlease install Docker for mac with this link\033[m \033[1;4;33mhttps://download.docker.com/mac/stable/Docker.dmg\033[m"
+    # open -a "Managed Software Center"
+    read -p $'\033[1mPress\033[m \033[1;7mENTER\033[0m \033[1mwhen you have successfully installed...\033[m'
+done
